6e496d1cec09b8985ef14abbbe1b7b58a10b75e0
deadXcriminal <arjunnknair@gmail.com>
2022-08-12 22:07:37 +0530

    updated

---
+++ assets/js/tailwindconfig.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'diadh' : {
+            100 : '#ece3da',
+            200 : '#e8ddd2',
+            300 : '#e4d7cb',
+            400 : '#e0d1c3',
+            500 : '#dcccbc',
+            600 : '#d8c6b4',
+            700 : '#D8C6B4'
+          }
+        }
+      }
+    }
+  }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,18 @@
-
 module.exports = {
-    plugins: [],
-  }
+  theme: {
+    extend: {
+      colors: {
+        'diadh' : {
+          100 : '#ece3da',
+          200 : '#e8ddd2',
+          300 : '#e4d7cb',
+          400 : '#e0d1c3',
+          500 : '#dcccbc',
+          600 : '#d8c6b4',
+          700 : '#D8C6B4'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
